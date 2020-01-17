d48f571ae2f81d6f32a30659090f55932859728f
Roman <s0547499@htw-berlin.de>
2020-01-17 22:44:37 +0100

    added basic cli parser

--- src/main/java/de/htw/ai/App.java
+++ src/main/java/de/htw/ai/App.java
@@ -1,9 +1,17 @@
 package de.htw.ai;
 
-public class App 
+import org.apache.commons.cli.ParseException;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CliConfiguration configuration = new CliConfiguration();
+
+        try {
+            configuration.parseArgs(args);
+        } catch (ParseException e) {
+            configuration.printHelp();
+        }
     }
 }
