2a719fe510489e490023f30d2ad016c3e9264a03
Roman <s0547499@htw-berlin.de>
2020-01-30 18:18:19 +0100

    NeoStatement class, integrated in rdfconverter

--- src/main/java/de/htw/ai/NeoIRI.java
+++ src/main/java/de/htw/ai/NeoIRI.java
@@ -3,14 +3,14 @@ package de.htw.ai;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NeoIRI {
+public class NeoIRI implements NeoElement {
     private String iri;
     private String ns;
     private String namespace;
 
     public NeoIRI(String iri, String ns, String namespace) {
         this.iri = iri;
-        this.ns = namespace;
+        this.ns = ns;
         this.namespace = namespace;
     }
 
