36d96a0df082744f87a9d60aa3051354e3fc8431
Roman <s0547499@htw-berlin.de>
2020-01-30 16:16:26 +0100

    integrated ontology handler in rdf converter

--- src/main/java/de/htw/ai/RdfConverter.java
+++ src/main/java/de/htw/ai/RdfConverter.java
@@ -11,11 +11,25 @@ public class RdfConverter {
     public static Map<String, Object> StatementToNodeMap(Statement statement) {
         Map<String, Object> node = new HashMap<>();
 
-        node.put("subject", statement.getSubject());
-        node.put("predicate", statement.getPredicate());
-        node.put("object", statement.getObject());
+        node.put("subject", resolveNamespace(statement.getSubject().stringValue()));
+        node.put("predicate", resolveNamespace(statement.getPredicate().stringValue()));
+        node.put("object", resolveNamespace(statement.getObject().stringValue()));
 
         return node;
+    }
+
+    private static String resolveNamespace(String iri) {
+        if (iri.contains("/")) {
+            String namespace = iri.substring(0, iri.lastIndexOf("/"));
+            String namespaceAbbreviation = OntologyHandler.getInstance().getOntologyKey(namespace);
+
+            if (namespaceAbbreviation != null)
+                return namespaceAbbreviation + ":" + iri.replace(namespace, "");
+
+            return OntologyHandler.getInstance().addOntology(namespace) + ":" + iri.replace(namespace, "");
+        }
+
+        return iri;
     }
 
     public static Collection<Map<String, Object>> StatementsToNodeMaps(Collection<Statement> statements) {
