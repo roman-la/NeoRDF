49755ee07de2ab0e8ba26377da2d109ee7fa646d
Roman <s0547499@htw-berlin.de>
2020-02-03 19:15:33 +0100

    reworked graphdb tests

--- src/test/java/de/htw/ai/db/GraphDatabaseTest.java
+++ src/test/java/de/htw/ai/db/GraphDatabaseTest.java
@@ -1,39 +1,32 @@
 package de.htw.ai.db;
 
 import de.htw.ai.App;
-import de.htw.ai.models.NeoElement;
 import de.htw.ai.models.NeoIRI;
 import de.htw.ai.models.NeoLiteral;
 import de.htw.ai.models.NeoStatement;
 import de.htw.ai.util.Configuration;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.neo4j.graphdb.Entity;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
+import org.junit.jupiter.api.*;
 import org.neo4j.graphdb.Result;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Map;
 
 public class GraphDatabaseTest {
 
     private static GraphDatabase db;
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    public void beforeEach() {
         App.config = new Configuration();
         App.config.setConfigValue("dbdirectory", "src/test/resources/db");
+
+        db = new GraphDatabase();
     }
 
-    @AfterAll
-    static void afterAll() throws IOException {
+    @AfterEach
+    public void afterEach() throws IOException {
         db.shutdown();
 
         FileUtils.deleteDirectory(new File("src/test/resources/db"));
@@ -41,82 +34,91 @@ public class GraphDatabaseTest {
 
     @Test
     public void inputStatementTest() {
-        NeoIRI subject = new NeoIRI("http://example.org/#roman", "ex", "http://example.org/");
-        NeoIRI predicate1 = new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-        NeoIRI object1 = new NeoIRI("http://xmlns.com/foaf/0.1/Person", "foaf", "http://xmlns.com/foaf/0.1/");
-        NeoStatement neoStatement1 = new NeoStatement(subject, predicate1, object1);
+        db.insertNeoStatement(new NeoStatement(
+                new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+                new NeoIRI("http://xmlns.com/foaf/0.1/Person", "foaf", "http://xmlns.com/foaf/0.1/")));
 
-        db = new GraphDatabase();
+        Assertions.assertTrue(db.executeQuery("MATCH (n) RETURN n;").resultAsString().contains("2 rows"));
 
-        db.insertNeoStatement(neoStatement1);
-        String countTest = db.executeQuery("MATCH (n) RETURN n;").resultAsString();
-        Assertions.assertTrue(countTest.contains("2 rows"));
+        db.insertNeoStatement(new NeoStatement(
+                new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://xmlns.com/foaf/0.1/name", "foaf", "http://xmlns.com/foaf/0.1/"),
+                new NeoLiteral("Roman L.")));
 
-        NeoIRI predicate2 = new NeoIRI("http://xmlns.com/foaf/0.1/name", "foaf", "http://xmlns.com/foaf/0.1/");
-        NeoLiteral object2 = new NeoLiteral("Roman L.");
-        NeoStatement neoStatement2 = new NeoStatement(subject, predicate2, object2);
+        Assertions.assertTrue(db.executeQuery("MATCH (n) RETURN n;").resultAsString().contains("3 rows"));
 
-        db.insertNeoStatement(neoStatement2);
-        countTest = db.executeQuery("MATCH (n) RETURN n;").resultAsString();
-        Assertions.assertTrue(countTest.contains("3 rows"));
+        db.insertNeoStatement(new NeoStatement(
+                new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://xmlns.com/foaf/0.1/name", "foaf", "http://xmlns.com/foaf/0.1/"),
+                new NeoLiteral("Roman L.")));
 
-        db.insertNeoStatement(neoStatement2);
-        countTest = db.executeQuery("MATCH (n) RETURN n;").resultAsString();
-        Assertions.assertTrue(countTest.contains("3 rows"));
+        Assertions.assertTrue(db.executeQuery("MATCH (n) RETURN n;").resultAsString().contains("3 rows"));
 
-        String resultSubject = db.executeQuery("MATCH (n:subject {iri: \"http://example.org/#roman\", ns: \"ex\", namespace: \"http://example.org/\"}) RETURN n;").resultAsString();
+        String resultSubject = db.executeQuery("MATCH (n {iri: \"http://example.org/#roman\", ns: \"ex\", namespace: \"http://example.org/\"}) RETURN n;").resultAsString();
+
         Assertions.assertTrue(resultSubject.contains("1 row"));
     }
 
     @Test
     public void restartDatabaseTest() {
-        NeoIRI subject = new NeoIRI("http://example.org/#roman", "ex", "http://example.org/");
-        NeoIRI predicate1 = new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-        NeoIRI object1 = new NeoIRI("http://xmlns.com/foaf/0.1/Person", "foaf", "http://xmlns.com/foaf/0.1/");
-        NeoStatement neoStatement1 = new NeoStatement(subject, predicate1, object1);
+        db.insertNeoStatement(new NeoStatement(
+                new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+                new NeoIRI("http://xmlns.com/foaf/0.1/Person", "foaf", "http://xmlns.com/foaf/0.1/")));
 
-        db = new GraphDatabase();
-
-        db.insertNeoStatement(neoStatement1);
-        String countTest = db.executeQuery("MATCH (n) RETURN n;").resultAsString();
-        Assertions.assertTrue(countTest.contains("2 rows"));
+        Assertions.assertTrue(db.executeQuery("MATCH (n) RETURN n;").resultAsString().contains("2 rows"));
 
         db.shutdown();
 
         db = new GraphDatabase();
 
-        countTest = db.executeQuery("MATCH (n) RETURN n;").resultAsString();
-        Assertions.assertTrue(countTest.contains("2 rows"));
+        Assertions.assertTrue(db.executeQuery("MATCH (n) RETURN n;").resultAsString().contains("2 rows"));
     }
 
     @Test
     public void extractNeoStatementsTest() {
-        NeoIRI subject = new NeoIRI("http://example.org/#roman", "ex", "http://example.org/");
-        NeoIRI predicate = new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-        NeoIRI object = new NeoIRI("http://xmlns.com/foaf/0.1/Person", "foaf", "http://xmlns.com/foaf/0.1/");
-        NeoLiteral literal = new NeoLiteral("test");
+        db.insertNeoStatement(new NeoStatement(
+                new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+                new NeoIRI("http://xmlns.com/foaf/0.1/Person", "foaf", "http://xmlns.com/foaf/0.1/")));
 
-        db = new GraphDatabase();
-
-        db.insertNeoStatement(new NeoStatement(subject, predicate, object));
-        db.insertNeoStatement(new NeoStatement(subject, predicate, literal));
+        db.insertNeoStatement(new NeoStatement(
+                new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+                new NeoLiteral("test")));
 
         Collection<NeoStatement> statements = db.extractNeoStatements("MATCH (s)-[p]->(o) RETURN s, p, o;");
 
         for (NeoStatement statement : statements) {
-            subject = (NeoIRI) statement.getSubject();
-            predicate = (NeoIRI) statement.getPredicate();
+            NeoIRI subject = (NeoIRI) statement.getSubject();
+            NeoIRI predicate = statement.getPredicate();
 
             Assertions.assertEquals("http://example.org/#roman", subject.getProperties().get("iri"));
             Assertions.assertEquals("rdf", predicate.getProperties().get("ns"));
 
             if (statement.getObject() instanceof NeoIRI) {
-                predicate = (NeoIRI) statement.getPredicate();
+                predicate = statement.getPredicate();
                 Assertions.assertEquals("rdf", predicate.getProperties().get("ns"));
             } else if (statement.getObject() instanceof NeoLiteral) {
-                literal = (NeoLiteral) statement.getObject();
+                NeoLiteral literal = (NeoLiteral) statement.getObject();
                 Assertions.assertEquals("test", literal.getValue());
             }
         }
+    }
+
+    @Test
+    public void executeQueryTest() {
+        db.insertNeoStatement(new NeoStatement(new NeoIRI("http://example.org/#roman", "ex", "http://example.org/"),
+                new NeoIRI("http://xmlns.com/foaf/0.1/name", "foaf", "http://xmlns.com/foaf/0.1/"),
+                new NeoLiteral("Roman L."))
+        );
+
+        Result r = db.executeQuery("MATCH (x)-[:predicate {iri: \"http://xmlns.com/foaf/0.1/name\"}]->(name)\nRETURN name");
+
+        String resultString = r.resultAsString();
+
+        Assertions.assertTrue(resultString.contains("{value:\"Roman L.\"}"));
+        Assertions.assertTrue(resultString.contains("1 row"));
     }
 }
