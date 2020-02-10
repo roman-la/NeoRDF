5c3fa83635ce9436b78890e7e2bf002e34d80a12
Roman <s0547499@htw-berlin.de>
2020-02-10 09:48:37 +0100

    test class deletes created file after all

--- src/test/java/de/htw/ai/config/NeoConfigurationTest.java
+++ src/test/java/de/htw/ai/config/NeoConfigurationTest.java
@@ -1,27 +1,36 @@
 package de.htw.ai.config;
 
 import org.apache.commons.cli.ParseException;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class NeoConfigurationTest {
 
+    private String configPath = "src/test/resources/configexample.txt";
     NeoConfiguration config;
-    String[] testArgs = new String[]{"-config", "src/test/resources/configexample.txt"};
+    String[] testArgs = new String[]{"-config", configPath};
 
     @BeforeEach
     public void beforeEach() {
         config = new NeoConfiguration();
     }
 
+    @AfterAll
+    public void afterAll() throws IOException {
+        Files.delete(Paths.get(configPath));
+    }
+
     @Test
     public void argsTest() {
         Assertions.assertDoesNotThrow(() -> config.parse(testArgs));
 
-        Assertions.assertThrows(ParseException.class, () -> config.parse(new String[] {"-unknown", "value"}));
+        Assertions.assertThrows(ParseException.class, () -> config.parse(new String[]{"-unknown", "value"}));
     }
 
     @Test
